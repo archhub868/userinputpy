97c4fcdb4ade01a3529772f5be55197ffc9a23bd
Habakkuk Onchiri <19738203-HabakkukOnchiri@users.noreply.replit.com>
2024-02-29 13:07:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+location = input("Where do you live? ")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
